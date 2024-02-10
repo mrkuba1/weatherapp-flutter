4d9ffd0cb660bc4fe7ec0c8586952b94e133fd61
Kubix <mr.jakubsierocki@gmail.com>
2024-02-10 19:23:27 +0100

    change home screen and add choice screen

---
+++ lib/models/city.dart
@@ -0,0 +1,75 @@
+class City {
+  bool isSelected;
+  final String name;
+  final String country;
+  final bool isDefault;
+
+  City({
+    required this.country,
+    required this.isSelected,
+    required this.name,
+    required this.isDefault,
+  });
+
+  static List<City> defaultCitiesList = [
+    City(
+      isSelected: false,
+      name: 'London',
+      country: 'United Kingdom',
+      isDefault: true,
+    ),
+    City(
+      isSelected: false,
+      name: 'Warsaw',
+      country: 'Poland',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Wien',
+      country: 'Austria',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Paris',
+      country: 'France',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Lisbon',
+      country: 'Portugal',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Stockholm',
+      country: 'Sweden',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Oslo',
+      country: 'Norway',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Berlin',
+      country: 'Germany',
+      isDefault: false,
+    ),
+    City(
+      isSelected: false,
+      name: 'Rome',
+      country: 'Italy',
+      isDefault: false,
+    ),
+  ];
+
+  static List<City> getSelectedCities() {
+    List<City> selectedCities = City.defaultCitiesList;
+    return selectedCities.where((city) => city.isSelected == true).toList();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,101 +1,50 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:weatherapp/widgets/thermometr.dart';
+import 'package:weatherapp/screens/choice_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        // appBar: AppBar(
-        //   backgroundColor: Colors.transparent,
-        // ),
-        body: Padding(
-            padding:
-                const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: Stack(
-                children: [
-                  Align(
-                      alignment: const AlignmentDirectional(3, -0.1),
-                      child: Container(
-                        height: 300,
-                        width: 300,
-                        decoration: const BoxDecoration(
-                            color: Colors.amber, shape: BoxShape.circle),
-                      )),
-                  Align(
-                      alignment: const AlignmentDirectional(-3, -0.1),
-                      child: Container(
-                        height: 300,
-                        width: 300,
-                        decoration: const BoxDecoration(
-                            color: Colors.amber, shape: BoxShape.circle),
-                      )),
-                  Align(
-                      alignment: const AlignmentDirectional(3, -1.5),
-                      child: Container(
-                        height: 300,
-                        width: 300,
-                        decoration: BoxDecoration(
-                            color: DateTime.now().hour < 12
-                                ? Colors.blue
-                                : Colors.purple,
-                            shape: BoxShape.circle),
-                      )),
-                  Align(
-                      alignment: const AlignmentDirectional(-3, -1.5),
-                      child: Container(
-                        height: 300,
-                        width: 300,
-                        decoration: BoxDecoration(
-                            color: DateTime.now().hour < 12
-                                ? Colors.blue
-                                : Colors.purple,
-                            shape: BoxShape.circle),
-                      )),
-                  BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
-                    child: Container(
-                      decoration:
-                          const BoxDecoration(color: Colors.transparent),
-                    ),
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/home.png"),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => ChoiceScreen()));
+              },
+              child: Container(
+                height: 100,
+                width: 250,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                ),
+                child: const Center(
+                  child: Text(
+                    "Push the button",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 20),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text('Moje miasto to Białystok',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300)),
-                        const SizedBox(height: 8.0),
-                        Text(
-                            DateTime.now().hour < 12
-                                ? 'Good Morning'
-                                : DateTime.now().hour < 18
-                                    ? 'Good Afternoon'
-                                    : 'Good Night',
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold)),
-                        const Thermometer(
-                          temperature: 20.0,
-                        ),
-
-                        // WeatherSceneWidget(weatherScene: WeatherScene.sunset)
-                      ],
-                    ),
-                  )
-                ],
+                ),
               ),
-            )));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
