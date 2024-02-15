dbce61ae5a2b2c98cf016542e280990fb6ab7420
Kubix <mr.jakubsierocki@gmail.com>
2024-02-15 13:20:57 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,6 @@ import 'package:weatherapp/bloc/weather_bloc.dart';
 import 'package:weatherapp/screens/home_screen.dart';
 
 main() async {
-  // final forecast = await WeatherClient().getForecast("Warsaw");
-  // var forecastDataHour = forecast.forecastday[1].hour[1].time;
-  // var currentHour = DateTime.now();
-  // print(forecastDataHour);
-  // print(currentHour.toString());
   runApp(const MyApp());
 }
 
