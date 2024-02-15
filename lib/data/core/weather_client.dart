e488e1c22b786e36f135d1e1ef9bda5abced5a73
Kubix <mr.jakubsierocki@gmail.com>
2024-02-15 13:20:47 +0100

    delete Current method

--- lib/data/core/weather_client.dart
+++ lib/data/core/weather_client.dart
@@ -2,16 +2,9 @@ import 'dart:convert';
 
 import 'package:weatherapp/data/core/base_client.dart';
 import 'package:weatherapp/data/core/weather_client_path.dart';
-import 'package:weatherapp/data/current/models/current.dart';
 import 'package:weatherapp/data/forecast/models/forecast.dart';
 
 class WeatherClient extends WABaseClient {
-  Future<Current> getCurrent(String location) async {
-    final response =
-        await get(WeatherClientPath.current.getUriCurrent(location));
-    return Current.fromJson(response.body);
-  }
-
   Future<Forecast> getForecast(String location) async {
     final response =
         await get(WeatherClientPath.forecast.getUriForecast(location));
